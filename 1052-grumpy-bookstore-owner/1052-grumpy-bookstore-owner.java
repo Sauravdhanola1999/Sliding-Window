a316e10b03613b97c567c78d4fc8599d40cee26c
Saurav Singh Dhanola <125140898+Sauravdhanola1999@users.noreply.github.com>
2026-08-04 20:02:24 +0530

    Time: 3 ms (96.07%), Space: 48.8 MB (18.84%) - LeetHub

---
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        int base =0;
+        for(int i=0; i<customers.length; i++){
+            if(grumpy[i]==0){
+                base+=customers[i];
+            }
+        }
+
+        int extra = 0;
+
+        for(int i=0; i<minutes; i++){
+            if(grumpy[i]==1){
+                base+=customers[i];
+            }
+        }
+
+        int maxGlumpy = extra;
+
+        for(int i=minutes; i<customers.length; i++){
+              if(grumpy[i-minutes]==1){
+                   extra-=customers[i-minutes];
+              }
+
+              if(grumpy[i]==1){
+                   extra+=customers[i];
+              }
+
+              maxGlumpy = Math.max(maxGlumpy, extra);
+        }
+        return maxGlumpy +base;
+    }
+}
